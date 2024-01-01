5038245861532ea8c645c88e71f6ac8bcd2b6050
Pankaj Makhijani <pankaj.makhijani@rishabhsoft.com>
2024-01-01 21:04:40 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    env: 'prod',
+    imageApiDev: 'https://source.unsplash.com/random',
+    imageApiProd: 'https://source.unsplash.com/collection/300',
+  };
