207acd26286769e6a9d452c1a3e802097dbe8333
Pankaj Makhijani <pankaj.makhijani@rishabhsoft.com>
2024-01-01 23:26:22 +0530

    changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    env: 'development',
+    imageApiDev: 'https://source.unsplash.com/random',
+    imageApiProd: 'https://source.unsplash.com/collection/1000',
+};  
---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: 'uat',
+  imageApiDev: 'https://source.unsplash.com/random',
+  imageApiProd: 'https://source.unsplash.com/collection/200',
+};
